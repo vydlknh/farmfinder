267e35909500a6e462d6e34dd8089604155660c3
Redsnapper12 <46903431+Redsnapper12@users.noreply.github.com>
2024-04-21 22:39:41 -0400

    Implemented Three Search Functions
    Added search by state, zip, county

---
+++ modules/search_functions.js
@@ -0,0 +1,101 @@
+export function search_by_zip(data, zipcode) {
+    var results = []
+    var col_idx = undefined
+
+    // Find zipcode column
+    for (let index = 0; index < data.length; index++) {
+        if(data[0][index] == 'postcode') {
+            col_idx = index
+            break
+        }
+    }
+
+    // Guard clause
+    if(col_idx == undefined) { 
+        console.error('No zipcode column found in file');
+        return null 
+    }
+
+    // Loop through the data and find all instances with the same zipcode
+    data.forEach(element => {
+        if(element[col_idx] == zipcode) {
+            results.push(element)
+        }
+    })
+
+    if(results.length == 0) {
+        alert('No farms found in ' + zipcode + ', please try a new zipcode')
+        return null
+    }
+    else {
+        return results
+    }
+}
+
+export function search_by_state(data, state) {
+    var results = []
+    var col_idx = undefined
+
+    // Find state_code column
+    for (let index = 0; index < data.length; index++) {
+        if(data[0][index] == 'state_code') {
+            col_idx = index
+            break
+        }
+    }
+
+    // Guard clause
+    if(col_idx == undefined) { 
+        console.error('No state_code column found in file');
+        return null 
+    }
+
+    // Loop through the data and find all instances with the same state_code
+    data.forEach(element => {
+        if(element[col_idx] == state) {
+            results.push(element)
+        }
+    })
+
+    if(results.length == 0) {
+        alert('No farms found in ' + state + ', please try a new state')
+        return null
+    }
+    else {
+        return results
+    }
+}
+
+export function search_by_county(data, county) {
+    var results = []
+    var col_idx = undefined
+
+    // Find state_code column
+    for (let index = 0; index < data.length; index++) {
+        if(data[0][index] == 'county') {
+            col_idx = index
+            break
+        }
+    }
+
+    // Guard clause
+    if(col_idx == undefined) { 
+        console.error('No county column found in file');
+        return null 
+    }
+
+    // Loop through the data and find all instances with the same state_code
+    data.forEach(element => {
+        if(element[col_idx] == county + ' County') {
+            results.push(element)
+        }
+    })
+
+    if(results.length == 0) {
+        alert('No farms found in ' + county + ', please try a new county')
+        return null
+    }
+    else {
+        return results
+    }
+}
--- src/search.js
+++ src/search.js
@@ -1,32 +1,43 @@
 import { parse } from '/modules/index.js'
+import { search_by_county, search_by_state, search_by_zip } from '/modules/search_functions.js'
 
-let button = document.getElementById('submit');
-button.addEventListener('click', search);
+let button = document.getElementById('submit')
+button.addEventListener('click', search)
 
 
 function search() {
 
-    let zipcode = document.getElementById('zipcode').value;
-    let product = document.getElementById('product').value;
+    let zipcode = document.getElementById('zipcode').value
 
-    fetch_csv('/data/farmersmarket_2024.csv')
-
-};
+    
+    fetch_csv('/data/geocoded/csa_2024_geocoded.csv')
+    .then(parsedData => {
+        // Search the parsed data for entries with matching zipcodes
+        //let results = search_by_zip(parsedData, zipcode)
+        //let results = search_by_state(parsedData, 'TX')
+        let results = search_by_county(parsedData, 'Putnam')
+        console.log(results)
+    })
+    .catch(error => {
+        console.error(error)
+    })
+}
 
 async function fetch_csv(path) {
     try {
-        const response = await fetch(path);
+        const response = await fetch(path)
         if(!response.ok) {
             throw new Error('Could not fetch CSV file at ${path}')
         }
 
         // Wait for text response then parse
         const data = await response.text()
-        const parsedData = parse(data);
+        const parsedData = parse(data)
 
-        console.log(parsedData); 
+        return parsedData
     }
     catch(error) {
-        console.error('Error parsing CSV at ${path}');
+        console.error('Error parsing CSV at ${path}')
+        return null
     }
-};
+}
