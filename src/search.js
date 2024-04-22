bdbeb01521dced96850c1a64afdf61e4dc23b6c0
Redsnapper12 <46903431+Redsnapper12@users.noreply.github.com>
2024-04-22 16:55:00 -0400

    Final Push Before Presentation

--- src/search.js
+++ src/search.js
@@ -1,9 +1,13 @@
 import { parse } from '/modules/index.js'
 import { search_by_state } from '/modules/search_functions.js'
-import {html, render} from 'https://esm.run/lit-html@1';
+import { render, html } from 'https://esm.run/lit-html@1'
 
 let button = document.getElementById('submit')
 button.addEventListener('click', search)
+
+var address_index
+var lat_index
+var lon_index
 
 
 // Search the database
@@ -21,6 +25,7 @@ function search() {
         // Search the parsed data for entries with matching zipcode
         let results = search_by_state(parsedData, state)
 
+        console.log(results);
         // Render new cards on the DOM
         renderCard(results)
     })
@@ -51,18 +56,29 @@ async function fetch_csv(path) {
 function get_file_by_organization_type(type) {
     switch (type) {
         case 'csa':
+            address_index = 249
+            lon_index = 7
+            lat_index = 6
             return '/data/geocoded/csa_2024_geocoded.csv'
         case 'farmersmarket':
-            return '/data/geocoded/csa_2024_geocoded.csv'
+            address_index = 47
+            lon_index = 5
+            lat_index = 4
+            return '/data/geocoded/farmersmarket_2024_geocoded.csv'
         case 'foodhub':
-            return '/data/geocoded/csa_2024_geocoded.csv'
-        case 'onfarmmarket':
-            return '/data/geocoded/csa_2024_geocoded.csv'
+            address_index = 206
+            lon_index = 7
+            lat_index = 6
+            return '/data/geocoded/foodhub_2024_geocoded.csv'
     }
 }
 
 function renderCard(listing) {
     var list = document.getElementById("displayListing")
+    
+    while (list.firstChild) {
+        list.removeChild(list.lastChild);
+      }
 
     const renderResult = (result) => `
     <div class="card m-4">
@@ -80,27 +96,26 @@ function renderCard(listing) {
         </div>
     
         <div class="content">
-            ${result[249]}
+            ${result[address_index]}
         </div>
+        <button class="button is-link is-fullwidth">Show On Map</button> 
         </div>
     </div>
     `
-
     const renderMap = (result) => html`
-    <gmp-map style="height: 95vh;" center="${result[7]},${result[6]}" zoom="14" map-id="DEMO_MAP_ID">
-        <gmp-advanced-marker position="${result[7]},${result[6]}" title="Listing Location"></gmp-advanced-marker>
+    <gmp-map style="height: 95vh;" center="${result[lon_index]},${result[lat_index]}" zoom="14" map-id="DEMO_MAP_ID">
+        <gmp-advanced-marker position="${result[lon_index]},${result[lat_index]}" title="Listing Location"></gmp-advanced-marker>
     </gmp-map>
     `
+
 
     for (let index = 0; index < listing.length; index++) {
         const element = listing[index];
         
         var elem = new DOMParser().parseFromString(renderResult(element), 'text/html')
-        list.appendChild(elem.firstChild)
-
-        if(index + 1 == listing.length) {
-            
-            render(renderMap(element), document.getElementById("displayMap"));
-        }
+        list.appendChild(elem.firstChild)  
     }
+
+    
+    render(renderMap(listing[0]), document.getElementById("displayMap"));
 }
